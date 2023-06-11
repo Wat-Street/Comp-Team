8430a351a29ca752764458f7747b16ae3c49e319
Leo Huang <leogjhuang@gmail.com>
2023-06-11 17:18:58 -0400

    Add driver and connection code

---
+++ gamma/arbitrage/driver.py
@@ -0,0 +1,10 @@
+# driver to connect to SQL server and store csv data per exchange and per asset in ExchangeData folder
+from sql_connection import SQLConnection
+from utility import query, segregateExchangeAndAssets
+
+if __name__ == "__main__":
+    SQLObject = SQLConnection()
+    connection = SQLObject.create_connection()
+    cursor = connection.cursor()
+
+    # connection successful (add operations to read data here)
